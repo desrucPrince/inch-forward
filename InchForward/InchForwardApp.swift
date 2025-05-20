0cdf3fceacd25047a4b86e6678d41fc42a6e61a2
Darrion Johnson <90425357+desrucPrince@users.noreply.github.com>
2025-05-20 12:48:23 -0500

    Initial Commit

---
+++ InchForward/InchForwardApp.swift
@@ -0,0 +1,17 @@
+//
+//  InchForwardApp.swift
+//  InchForward
+//
+//  Created by Darrion Johnson on 5/20/25.
+//
+
+import SwiftUI
+
+@main
+struct InchForwardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InchForwardTests/InchForwardTests.swift
@@ -0,0 +1,17 @@
+//
+//  InchForwardTests.swift
+//  InchForwardTests
+//
+//  Created by Darrion Johnson on 5/20/25.
+//
+
+import Testing
+@testable import InchForward
+
+struct InchForwardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
